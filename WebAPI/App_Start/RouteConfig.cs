3364c4b3eb34faf1a64de44c1ff2cb05551a7a51
oscarporras02 <oscarrussi@outlook.com>
2021-02-04 19:14:05 -0500

    add method to test the use of another api

--- WebAPI/App_Start/RouteConfig.cs
+++ WebAPI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebAPI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Users", action = "Get", id = UrlParameter.Optional }
+                defaults: new { controller = "Users", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Http.Cors;
 using System.Web.Mvc;
 
@@ -11,13 +15,63 @@ namespace WebAPI.Controllers
 {
     public class UsersController : Controller
     {
-        // GET: Users
-        public ActionResult Index()
+        static HttpClient client = new HttpClient();
+        public async System.Threading.Tasks.Task<JObject> Index()
         {
-            
-            return View();
+            string product;
+            JObject p = new JObject();
+            using (HttpResponseMessage response = await client.GetAsync("http://api.openweathermap.org/data/2.5/weather?q=bogota&appid=a71219e79a6b01978ac3a9f3ffccca37"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    product = await response.Content.ReadAsStringAsync();
+                    p = (JObject)JsonConvert.DeserializeObject(product);
+                }
+            }
+            return p;
         }
 
+        [HttpPost]
+        public async System.Threading.Tasks.Task<string> Index2()
+        {
+            JObject p = new JObject();
+            string product="";
+            //p["name"] = "aaaa";
+            //p["credit"] = 5000;
+            var stringContent = new StringContent(p.ToString());
+            using (HttpResponseMessage response = await client.PostAsync("https://roulettegame.azurewebsites.net/game/create", stringContent))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    product = await response.Content.ReadAsStringAsync();
+                }
+            }
+            return product;
+        }
+
+        [HttpPost]
+        public async System.Threading.Tasks.Task<string> Index3()
+        {
+            string product = "";
+            JObject p = new JObject();
+            p["name"] = "aaaa";
+            p["credit"] = 5000;
+            //var stringContent = new StringContent(p.ToString());
+            //var parameters = new Dictionary<string, string> { { "name", "aaa" }, { "credit", "5000"} };
+            //var encodedContent = new FormUrlEncodedContent(parameters);
+            var jsonInString = JsonConvert.SerializeObject(p);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(jsonInString);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            using (HttpResponseMessage response = await client.PostAsync("https://roulettegame.azurewebsites.net/player/create", byteContent))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    product = await response.Content.ReadAsStringAsync();
+                }
+            }
+            return product;
+        }
         //[System.Web.Http.Route("api/users")]
         //[System.Web.Http.HttpGet]
         public string Get()
